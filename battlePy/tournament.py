bc4a2cb46e3f6834e5048686d8eee070106031d4
Kevin Yokley <kyokley2@gmail.com>
2015-07-11 07:25:45 -0500

    Adding leaderboard

--- battlePy/tournament.py
+++ battlePy/tournament.py
@@ -1,6 +1,7 @@
 from series import Series
 from itertools import combinations
 from blessings import Terminal
+from tabulate import tabulate
 
 class Tournament(object):
     def __init__(self,
@@ -25,7 +26,9 @@ class Tournament(object):
         self.term = Terminal()
 
     def run(self):
-        self.results = dict([(player, [0, []]) for player in self.players])
+        self.results = dict([(player, [{'win': 0,
+                                        'lose': 0,
+                                        'draw': 0}, []]) for player in self.players])
         self.series = [Series(x[0](),
                               x[1](),
                               self.numberOfGames,
@@ -42,14 +45,33 @@ class Tournament(object):
             if self.showVisualization:
                 print self.term.clear
             result = series.start()
-            if result:
-                self.results[type(result[0])][0] += 1
+            if result is not None:
+                self.results[type(result[0])][0]['win'] += 1
+                self.results[type(result[1])][0]['lose'] += 1
+
                 self.results[type(result[0])][1].append(type(result[1]).__name__)
+            else:
+                # A null result means we got a draw
+                self.results[type(result[0])][0]['draw'] += 1
+                self.results[type(result[1])][0]['draw'] += 1
 
-        self.displayResults()
+            self.displayLeaderBoard()
 
-    def displayResults(self):
-        rankings = sorted(self.results.items(), key=lambda x: -x[1][0])
+        self.finalResults()
+
+    def displayLeaderBoard(self):
+        rankings = sorted(self.results.items(), key=lambda x: -x[1][0]['win'])
+        data = []
+        for idx, ranking in enumerate(rankings):
+            data.append(('%s: %s' % (idx + 1, ranking[0].__name__),
+                         ranking[1][0]['win'],
+                         ranking[1][0]['lose'],
+                         ranking[1][0]['draw'],))
+        table = tabulate(data, headers=['Player', 'Wins', 'Losses', 'Draws'])
+        print table
+
+    def finalResults(self):
+        rankings = sorted(self.results.items(), key=lambda x: -x[1][0]['win'])
         for idx, ranking in enumerate(rankings):
             print '%s: %s' % (idx + 1, ranking[0].__name__)
             print '   with wins against %s' % ranking[1][1]
