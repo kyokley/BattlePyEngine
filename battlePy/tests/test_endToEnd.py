6896b804158f91eb4c76b07a41a02b6d0471f5b1
Kevin Yokley <kyokley2@gmail.com>
2015-07-14 07:07:26 -0500

    Adding functional test

---
+++ battlePy/tests/test_endToEnd.py
@@ -0,0 +1,16 @@
+import unittest
+from battlePy.series import Series
+from battlePy.random_player import RandomPlayer
+from battlePy.improved_random_player import ImprovedRandomPlayer
+
+class TestPlay(unittest.TestCase):
+    def setUp(self):
+        self.series = Series(RandomPlayer(),
+                             ImprovedRandomPlayer(),
+                             numberOfGames=11,
+                             debug=False,
+                             showVisualization=True,
+                             visualizationInterval=.001)
+
+    def test_play(self):
+        self.series.start()
